0341e39f9784462ee41e83ca3be4780dd7736a4b
Shocker55 <bialetti360@gmail.com>
2023-12-07 14:55:38 +0900

    feat: DIによって依存関係を分離したユニットテスト
    SampleController は SampleServiceImpl という具象クラスではなく、SampleService というインタフェースに依存する。
    つまり、SampleController のテストで SampleService の実装を差し替えられる。そのため、SampleController のロジックの検証に注力できる。

---
+++ src/test/kotlin/basicofspringboot/SampleControllerTest.kt
@@ -0,0 +1,34 @@
+package basicofspringboot
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+
+class SampleControllerTest {
+    @Test
+    fun `SampleService を DI したテスト`() {
+        /**
+         * given:
+         * - SampleService.execute のスタブを作成
+         * - スタブ化した SampleService で SampleController をインスタンス化することで、状態が固定する
+         */
+        val sampleService = object : SampleService {
+            override fun execute(): ServiceDto = ServiceDto(ServicePerson("Bob", 24))
+        }
+        val sampleController = SampleController(sampleService)
+
+        /**
+         * when:
+         * - メソッドの呼び出し(操作)は、スタブの利用前と利用後で変わらない
+         */
+        val actual = sampleController.getPerson()
+
+        /**
+         * then:
+         * - スタブ化した値から ResponseEntity が生成されていることを確認する
+         */
+        val expected = ResponseEntity(PersonResponse(Person(name = "Bob", age = 24)), HttpStatus.OK)
+        assertEquals(actual, expected)
+    }
+}
